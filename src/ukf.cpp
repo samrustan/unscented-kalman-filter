354a88897cdbe34c1a115a2a0ee1cf4fa7fe8702
Sam Rustan <samrustan@gmail.com>
2017-07-28 09:46:52 -0500

    added init params, values

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -24,10 +24,10 @@ UKF::UKF() {
   P_ = MatrixXd(5, 5);
 
   // Process noise standard deviation longitudinal acceleration in m/s^2
-  std_a_ = 30;
+  std_a_ = 2.0;
 
   // Process noise standard deviation yaw acceleration in rad/s^2
-  std_yawdd_ = 30;
+  std_yawdd_ = 0.4;
 
   // Laser measurement noise standard deviation position1 in m
   std_laspx_ = 0.15;
@@ -45,12 +45,27 @@ UKF::UKF() {
   std_radrd_ = 0.3;
 
   /**
-  TODO:
+  DONE:
 
   Complete the initialization. See ukf.h for other member properties.
 
   Hint: one or more values initialized above might be wildly off...
+    --std_a_ & std_yawdd_ were set to 30, yes wildly
+    --these params tuned manually for results 
   */
+  n_x_ = 5;
+  n_aug_ = 7;
+  lambda_ = 3 - n_aug_;
+  P_ << 0.15,    0,    0,    0,    0, 
+           0, 0.15,    0,    0,    0, 
+           0,    0,    1,    0,    0, 
+           0,    0,    0,    1,    0,
+           0,    0,    0,    0,    1;
+
+  Xsig_pred_ = MatrixXd(n_x_, 2*n_aug_ + 1);
+ 
+  weights_ = VectorXd(2*n_aug_ + 1);
+  
 }
 
 UKF::~UKF() {}
