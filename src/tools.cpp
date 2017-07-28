baf5c99f72b344b93704b7a191d21eb6a87ab879
Sam Rustan <samrustan@gmail.com>
2017-07-28 09:33:52 -0500

    fixed typo

--- src/tools.cpp
+++ src/tools.cpp
@@ -15,4 +15,31 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
   TODO:
     * Calculate the RMSE here.
   */
+
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  if(estimations.size() != ground_truth.size() || estimations.size() == 0)
+  {
+    std::cout << "Invalid estimation or ground_truth data" << std::endl;
+    return rmse;
+  }
+
+  // Accumulate squared residuals
+  for (int i = 0; i < estimations.size(); i++)
+  {
+    VectorXd residual = estimations[i] - ground_truth[i];
+ 
+    residual = residual.array() * residual.array();
+
+    rmse += residual;
+  }
+
+  // mean
+  rmse /= estimations.size();
+
+  // sq rt
+  rmse = rmse.array().sqrt();
+  
+  return rmse;
 }
